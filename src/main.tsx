994e729d917f7e9928c45a1614aa4a132d3e2069
Omar Tarek Ali <Omar.Tarek.Ali@globaction.com>
2025-01-05 20:05:53 +0200

    Adding bootstrap + lists

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import H1 from './Header.tsx'
-
+import 'bootstrap/dist/css/bootstrap.css'
+// import App from './App.tsx'
+// import H1 from './Header.tsx'
+import ListGroup from './Componenets/ListGroup'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <H1 />
+    <ListGroup/>
   </StrictMode>,
 )
